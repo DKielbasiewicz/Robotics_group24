5034ab40585c5c3e277e8ef94ec39c227bd92f96
Kiełbasiewicz, D.K. (Daniel) <daniel.kielbasiewicz@ru.nl>
2025-04-16 14:26:40 +0200

    change function

--- lineMaze.py
+++ lineMaze.py
@@ -1,5 +1,6 @@
 from robots import *
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+from time import sleep
 
 client = RemoteAPIClient()
 sim = client.require("sim")
@@ -13,13 +14,13 @@ left_motor, right_motor:
             It gives certain speed to the left wheel (we can steer the robot)
 
 color_sensor:
-    Colorsensor is used to detect the color of the line
+    Color sensor is used to detect the color of the line
 """
 
-left_motor = Motor(sim, DeviceNames.MOTOR_LEFT_LINE, Direction.CLOCKWISE)
-right_motor = Motor(sim, DeviceNames.MOTOR_RIGHT_LINE, Direction.CLOCKWISE)
+left_motor = Motor(sim, DeviceNames.MOTOR_LEFT_LINE, Direction.CLOCKWISE) # left wheel engine
+right_motor = Motor(sim, DeviceNames.MOTOR_RIGHT_LINE, Direction.CLOCKWISE) # right wheel engine
 # Color sensor is the 
-color_sensor = ImageSensor(sim, DeviceNames.IMAGE_SENSOR_LINE)
+color_sensor = ImageSensor(sim, DeviceNames.IMAGE_SENSOR_LINE) #what robot sees
 
 def is_red_detected(color_sensor):
     """
@@ -57,10 +58,12 @@ def turn(image_array):
     count_left = np.sum(left_half == 23)
     count_right = np.sum(right_half == 23)
 
-    if count_right > count_left:
+    if count_right < count_left:
         return 0  # Turn left
-    else:
+    elif count_left < count_right:
         return 2 # Turn right
+    # else:
+    #     return 4
 
 def follow_line():
     """
@@ -70,18 +73,22 @@ def follow_line():
     image = color_sensor.get_image()
     reflection = color_sensor.reflection() # Gets the reflection from the image
     print(reflection)
-    turns = turn(image)
+    turn_decision = turn(image)
     # left_motor.run(speed=5) # Runs the left motor at speed=5
     # right_motor.run(speed=5) # Runs the right motor at speed=5
-    if turns == 1:
+    if turn_decision == 1:
         left_motor.run(speed=5)
         right_motor.run(speed=5)
-    elif turns == 0:
-        right_motor.run(speed=7)
-        left_motor.run(speed=3)
-    else:
-        left_motor.run(speed=7)
-        right_motor.run(speed=3)
+    elif turn_decision == 0:
+        right_motor.run(speed=5)
+        left_motor.run(speed=0.5)
+    elif turn_decision == 2:
+        left_motor.run(speed=5)
+        right_motor.run(speed=0.5)
+    # elif turn_decision == 4:
+    #     left_motor.run(speed=3)
+    #     right_motor.run(speed=0)
+        
 
 # Starts coppeliasim simulation if not done already
 sim.startSimulation()
